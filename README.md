a9223b41fcc9bb5c3e160cd236e0bdc13c218dad
gerardo Alvarez <1gerardo3@gmail.com>
2022-12-04 16:37:28 -0800

    add images to coresel

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Trip-Timer
+# Trip-Timer.
+
+## description:
+
+## Usage:
+
+## URL:
+https://1gerardo3.github.io/Trip-Timer/
+
+## ScreeShot:
--- assests/scrip.js
+++ assests/scrip.js
@@ -30,3 +30,23 @@
     }
     }, 1000);
 
+    const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+
+    const alert = (message, type) => {
+      const wrapper = document.createElement('div')
+      wrapper.innerHTML = [
+        `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+        `   <div>${message}</div>`,
+        '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+        '</div>'
+      ].join('')
+    
+      alertPlaceholder.append(wrapper)
+    }
+    
+    const alertTrigger = document.getElementById('liveAlertBtn')
+    if (alertTrigger) {
+      alertTrigger.addEventListener('click', () => {
+        alert('Nice, now you know carlos is gay and the 9ers suck ass.', 'success')
+      })
+    }
